94790b0b32b9a3ebc8279dbe3100b60a526d306d
Alistair Willis <alistairrwillis@gmail.com>
2016-12-29 16:33:02 +0900

    add template files

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ var app = express();
 var port = process.env.PORT || 5000; 
 
 app.use(express.static('public')); 
-app.use(express.static('src/views'));
+app.set('views', './src/views'); 
+// app.set('view engine', 'jade'); 
+
+var handlebars = require('express-handlebars')
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine', '.hbs'); 
 
 app.get('/', function(req, res) {
-    res.send('hello world');
+    res.render('index', {title: 'Hello from render', list: ['a', 'b']});
 });
 
 app.get('/books', function(req, res) {
