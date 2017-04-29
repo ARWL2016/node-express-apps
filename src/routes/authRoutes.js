f88e6e352577a1b5bc9de3062d1c1fb4dad0baef
Alistair Willis <alistairrwillis@gmail.com>
2017-04-29 22:23:12 +0900

    complete authentication

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var authRouter = express.Router(); 
 var mongodb = require('mongodb').MongoClient; 
+var passport = require('passport'); 
 
 var router = function() {
   authRouter.route('/signUp')
     .post(function(req, res) {
       console.log(req.body); 
-      
       var url = 'mongodb://localhost:27017/libraryApp';
       mongodb.connect(url, function(err, db) {
         var collection = db.collection('users'); 
@@ -14,21 +14,31 @@ var router = function() {
           username: req.body.userName, 
           password: req.body.password
         }; 
-        collection.insert(user, function(err, results) {
-          req.logIn(results.ops[0], function () {
-          res.redirect('/auth/profile')
-          });
-        })
 
-      }); 
-
-
-     
+        collection.insert(user, 
+          function(err, results) {
+            req.logIn(results.ops[0], function () {
+              res.redirect('/auth/profile');
+            });
+          })
+        });      
     });
-    authRouter.route('/profile')
-      .get(function(req, res) {
-        res.json(req.user); 
-      })
+  authRouter.route('/signIn')
+    .post(passport.authenticate('local', {
+      failureRedirect: '/'
+    }), function (req, res) {
+      res.redirect('/auth/profile'); 
+    }); 
+  authRouter.route('/profile')
+    .all(function(req, res, next) {
+      if(!req.user) {
+        res.redirect('/');
+      }
+      next();
+    })
+    .get(function(req, res) {
+      res.json(req.user); 
+    })
 
     return authRouter; 
 }
